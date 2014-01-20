850d2ecaf04ca340832d01f0ea4d4743e8e8419b
renjie120 <lishuiqing110@163.com>
2014-01-20 08:49:36 +0800

    提交本地文件

--- src/com/renjie/Login.java
+++ src/com/renjie/Login.java
@@ -15,6 +15,7 @@ import android.widget.EditText;
 
 /**
  * 登录系统主界面.
+ * 
  * @author lsq
  * 
  */
@@ -24,6 +25,7 @@ public class Login extends BaseActivity {
 	private EditText usernameText;
 	private CheckBox showPass;
 	private EditText passwordText;
+	private static boolean debug = true;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -32,35 +34,37 @@ public class Login extends BaseActivity {
 		setContentView(R.layout.login);
 		usernameText = (EditText) findViewById(R.id.usertext);
 		passwordText = (EditText) findViewById(R.id.passtext);
-		loginBtn = (Button) findViewById(R.id.loginbtn); 
-		showPass = (CheckBox) findViewById(R.id.showpass); 
+		loginBtn = (Button) findViewById(R.id.loginbtn);
+		showPass = (CheckBox) findViewById(R.id.showpass);
 		quitBtn = (Button) findViewById(R.id.quitbtn);
-		//设置密码默认为不可见
-		passwordText
-		.setTransformationMethod(PasswordTransformationMethod
-				.getInstance()); 
-		//调用绑定事件的私有方法。
-		prepareListener();  
+		// 设置密码默认为不可见
+		passwordText.setTransformationMethod(PasswordTransformationMethod
+				.getInstance());
+		// 调用绑定事件的私有方法。
+		prepareListener();
+
+		if (debug)
+			alert("测试状态！");
 	}
 
 	protected void prepareListener() {
 		showPass.setOnCheckedChangeListener(new CheckBox.OnCheckedChangeListener() {
-					public void onCheckedChanged(CompoundButton buttonView,
-							boolean isChecked) {
-						if (showPass.isChecked()) {
-							// 设置密码框可见
-							passwordText
-									.setTransformationMethod(HideReturnsTransformationMethod
-											.getInstance());
-						} else {
-							// 设置密码不可见
-							passwordText
-									.setTransformationMethod(PasswordTransformationMethod
-											.getInstance());
-						}
-					}
+			public void onCheckedChanged(CompoundButton buttonView,
+					boolean isChecked) {
+				if (showPass.isChecked()) {
+					// 设置密码框可见
+					passwordText
+							.setTransformationMethod(HideReturnsTransformationMethod
+									.getInstance());
+				} else {
+					// 设置密码不可见
+					passwordText
+							.setTransformationMethod(PasswordTransformationMethod
+									.getInstance());
+				}
+			}
 
-				});
+		});
 
 		quitBtn.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
@@ -70,11 +74,12 @@ public class Login extends BaseActivity {
 
 		loginBtn.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
-				SharedPreferences settings = getSharedPreferences(Tool.CONFIG, 0);
+				SharedPreferences settings = getSharedPreferences(Tool.CONFIG,
+						0);
 				String us = usernameText.getText().toString();
-				String pas = passwordText.getText().toString();  
+				String pas = passwordText.getText().toString();
 				if (settings.getString(Tool.USERNAME, "user").equals(us)) {
-					if (settings.getString(Tool.PASS, "1").equals(pas)) {
+					if (debug || settings.getString(Tool.PASS, "1").equals(pas)) {
 						Intent openUrl = new Intent();
 						openUrl.setClass(Login.this, NewHomePage.class);
 						startActivity(openUrl);
